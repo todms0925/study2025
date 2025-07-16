44154b5b2f88ee47e4aa96f2ed1dd50fd42fb247
todms0925 <todms0925@gmail.com>
2025-07-16 14:28:26 +0900

    responsiver

---
+++ javascript/js.js
@@ -0,0 +1,14 @@
+//한줄주석
+// 제이쿼리 선언
+
+// 화면을 열었을 때, 다음과 같은 일들이 일어나라
+
+$(document).ready(function () {
+
+    // 실행문 작성
+    // 클래스 toggle을 클릭했을때, 클래스 gng가 슬라이드 Toggle되어라
+
+    $('.toggle').click(function(){
+        $('.gnb').sliedetoggle()
+    }
+})
